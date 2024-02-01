9ac4deb4f479b48cbaf4504c6b6196def9d860cd
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-02-01 05:39:26 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+  const gifts = ["teddy bear", "drone", "doll"];
+
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      debugger;
+    }
+  
+    return gifts;
+  }
+  
+  wrapGifts(gifts);
+
+  const thankYouCardsFor =["name1", "name2", "name3",]
+  function writeCards(thankYouCardsFor,eventName){
+    const messages= []
+     for (let i = 0; i < thankYouCardsFor.length; i++ ){
+        console.log (`Thank you, ${thankYouCardsFor[i]} `) 
+        messages.push(`Thank you, ${thankYouCardsFor[i]}, for the wonderful surprise gift!`)
+    } 
+    return(messages)
+  }
+
+
+ 
+  let  number = 10
+  function countDown(number){
+    
+    while(number>=0) {
+        console.log(number);
+        number--
+     }
+  }
